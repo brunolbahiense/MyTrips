4ecceccc762f271c01b2cb7bea9694c37e77b2d6
Bruno Luís Bahiense <brunolbahiense@LAPTOP-38VATNKK.localdomain>
2021-03-23 16:43:51 -0300

    graphql added

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,34 @@
-import AboutTemplate from 'templates/About'
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
+import { useRouter } from 'next/dist/client/router'
+import PageTemplate from 'templates/Pages'
 
-const About = () => <AboutTemplate />
+export default function AboutPage() {
+  const router = useRouter()
 
-export default About
+  if (router.isFallback) return null
+
+  return <PageTemplate />
+}
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => ({ params: { slug } }))
+
+  return { paths, fallback: true }
+}
+
+/* export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
+} */
+// getStaticPaths => serve para gerar as urls em build time /about, /trip/petropolis
+// getStaticProps => serve para buscar dados da página (props) - build time - estático
+// getServerSideProps => serve para buscar dados da página (props) - runtime - toda requisição (bundle fica no server)
+// getInitialProps => serve para buscar dados da página (props) - runtime - toda requisição (bundle também vem para o client) - hydrate
